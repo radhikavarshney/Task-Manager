661bab9e839a24fc4d4ed5f75c1842831cbba0ac
radhikavarshney <rvarshney2001@gmail.com>
2022-12-09 07:54:45 +0530

    2nd commit

--- 1st task manager/starter/app.js
+++ 1st task manager/starter/app.js
@@ -1,12 +1,17 @@
 const exp = require('express')
 const app = exp()
-const tasks = require('./oute/tasks')
+const tasks = require('./route/tasks')
 
 const port = 3000
+
+app.use(exp.json())
+
 
 app.get('/hello',(req,res)=>{
     res.send('Task Manager App')
 })
+
+app.use('/api/v1/tasks',tasks)
 
 
 app.listen(port,console.log(`User hit the server ${port}...`)
--- 1st task manager/starter/route/tasks.js
+++ 1st task manager/starter/route/tasks.js
@@ -1,3 +1,8 @@
 const exp = require('express')
 const router = exp.Router()
 
+const {getAllTasks}= require('../controller/tasks.js')
+
+router.route('/').get(getAllTasks)
+
+module.exports = router
