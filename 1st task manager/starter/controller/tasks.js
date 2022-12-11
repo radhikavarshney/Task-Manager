b579bc84a5b35f301187f13347c87b1d79426342
radhikavarshney <rvarshney2001@gmail.com>
2022-12-11 09:00:02 +0530

    6th commit

--- 1st task manager/starter/controller/tasks.js
+++ 1st task manager/starter/controller/tasks.js
@@ -1,8 +1,8 @@
-const Task =require('../models/tasks')
+const Task =require('../models/Task')
 
 const getAllTasks= async (req,res)=>{
 try {
-    const tasks = await tasks.find({})
+    const tasks = await Task.find({})
     res.status(200).json({tasks})
     
 } catch (error) {
@@ -23,8 +23,19 @@ const createTask = async (req,res)=>{
         
     }}
 
-const getTask = (req,res)=>{
-    res.json({id:req.params.id})
+const getTask = async (req,res)=>{
+try {
+    const {id:taskID} =req.params
+    const task = await Task.findOne({_id:taskID})
+    if(!task){
+        return res.status(404).json({msg:`no task with id: ${taskID}`})
+    }
+    
+    res.status(200).json({task})
+} catch (error) {
+    res.status(500).json({msg:error})
+}
+
 }
 const updateTask = (req,res)=>{
     res.send('update task')
