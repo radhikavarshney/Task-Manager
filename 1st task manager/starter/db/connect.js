e2e7a4d9ddaaa0951e8543044b8dfc8e4bf7e301
radhikavarshney <rvarshney2001@gmail.com>
2022-12-15 20:34:32 +0530

    11th commit

--- 1st task manager/starter/db/connect.js
+++ 1st task manager/starter/db/connect.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose')
 const connectDB = (url)=>{
     return mongoose
     .connect(url,{
-        useNewUrlParser:true,
-        useCreateIndex:true,
-        useFindAndModify:false,
-        useUnifiedTopology:true
+        // useNewUrlParser:true,
+        // useCreateIndex:true,
+        // useFindAndModify:false,
+        // useUnifiedTopology:true
     })
 }
 
