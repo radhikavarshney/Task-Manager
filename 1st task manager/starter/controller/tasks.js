e1b28a2f1c4db085e328cee137d63b86b5d4e1d2
radhikavarshney <rvarshney2001@gmail.com>
2022-12-16 17:12:34 +0530

    12th commit

--- 1st task manager/starter/controller/tasks.js
+++ 1st task manager/starter/controller/tasks.js
@@ -1,13 +1,13 @@
-const { findOneAndRemove, findOneAndDelete } = require('../models/Task')
+// const { findOneAndRemove, findOneAndDelete } = require('../models/Task')
 const Task =require('../models/Task')
 
 const getAllTasks= async (req,res)=>{
 try {
     const tasks = await Task.find({})
-    // res.status(200).json({tasks})
+    res.status(200).json({tasks})
     // res.status(200).json({tasks,amount:tasks.length})
 
-    res.status(200).json({status:'success',data:{tasks, nbHits:tasks.length}})
+    // res.status(200).json({status:'success',data:{tasks, nbHits:tasks.length}})
 
     
 } catch (error) {
@@ -61,17 +61,17 @@ const deleteTask =async (req,res)=>{
 const updateTask = async (req,res)=>{
    try {
     const {id:taskID}=req.params
-     const task = await Task.findOneAndUpdate({_id:taskID},req.body,
-        {
-            new:true,
-            runValidators:true
-        })
-     if(!task)
-     {
-         return res.status(404).json({msg:`no taask with id: ${taskID}`})
-     }
+    //  const task = await Task.findOneAndUpdate({_id:taskID},req.body,
+    //     {
+    //         new:true,
+    //         runValidators:true
+    //     })
+    //  if(!task)
+    //  {
+    //      return res.status(404).json({msg:`no taask with id: ${taskID}`})
+    //  }
      
-     res.status(200).json({task})
+     res.status(200).json({id:taskID,data:req.body})
  } catch (error) {
      res.status(500).json({msg:error})
  }
